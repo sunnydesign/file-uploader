1437984ff929b88a829095ee4fa4b5c6e0ab1791
Sergey Smagin <mail@sergey.perm.ru>
2020-02-18 07:58:36 +0000

    Fix migrations

--- usr/local/bin/run-php-fpm.sh
+++ usr/local/bin/run-php-fpm.sh
@@ -2,7 +2,8 @@
 
 env | perl -pe 's/(.+?)=(.*)/env[\1]=\$\1/' > /etc/php/7.2/fpm/pool.d/env.env
 
+cd /var/www/api.kubia.com/
+/usr/bin/php ./migrate.php >> /var/log/migrate.log
+
 /usr/sbin/php-fpm7.2 -F -y /etc/php/7.2/fpm/php-fpm.conf
 
-cd /var/www/api.kubia.com/
-php ./migrate.php
